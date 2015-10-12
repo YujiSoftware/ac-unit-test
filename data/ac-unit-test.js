8a436bed52cf7f68660db9bf5bb048e31f137f48
YujiSoftware <yuji.software+github@gmail.com>
2015-10-12 23:52:11 +0900

    Refactored JUnitTest Code

--- data/ac-unit-test.js
+++ data/ac-unit-test.js
@@ -36,42 +36,43 @@ self.on("click", function(){
   function createJUnit(io){
     var text = [];
     
-    text.push("import static org.hamcrest.CoreMatchers.is;");
-    text.push("");
-    text.push("import java.io.ByteArrayInputStream;");
-    text.push("import java.io.ByteArrayOutputStream;");
-    text.push("import java.io.InputStream;");
-    text.push("import java.io.PrintStream;");
-    text.push("");
-    text.push("import org.junit.Assert;");
-    text.push("import org.junit.Test;");
-    text.push("");
-    text.push("public class MainTest {");
-    text.push("");
+    text.push('import static org.hamcrest.CoreMatchers.is;');
+    text.push('');
+    text.push('import java.io.ByteArrayInputStream;');
+    text.push('import java.io.ByteArrayOutputStream;');
+    text.push('import java.io.PrintStream;');
+    text.push('');
+    text.push('import org.junit.Assert;');
+    text.push('import org.junit.Test;');
+    text.push('');
+    text.push('public class MainTest {');
+    text.push('');
     
     for(var i = 0; i < io.length; i++){
-      text.push("	@Test");
-      text.push("	public void " + io[i].name + "() {");
-      text.push("		String input = \"" + io[i].input.trim("\n").replace(/\n/g, "\\n") + "\";");
-      text.push("		String output = \"" + io[i].output.trim("\n").replace(/\n/g, "\\n") + "\";");
-      text.push("");
-      text.push("		assertIO(input, output);");
-      text.push("	}");
-      text.push("");
+      text.push('	@Test');
+      text.push('	public void ' + io[i].name + '() {');
+      text.push('		String input = ');
+      text.push('			"' + io[i].input.trim("\n").replace(/\n/g, '" + System.lineSeparator() +\n			"') + '";');
+      text.push('		String output = ');
+      text.push('			"' + io[i].output.trim("\n").replace(/\n/g, '" + System.lineSeparator() + \n			"') + '";');
+      text.push('');
+      text.push('		assertIO(input, output);');
+      text.push('	}');
+      text.push('');
     }
     
-    text.push("	private void assertIO(String input, String output) {");
-    text.push("		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());");
-    text.push("		System.setIn(in);");
-    text.push("");
-    text.push("		ByteArrayOutputStream out = new ByteArrayOutputStream();");
-    text.push("		System.setOut(new PrintStream(out));");
-    text.push("");
-    text.push("		Main.main(new String[0]);");
-    text.push("");
-    text.push("		Assert.assertThat(output + \"\\r\\n\", is(out.toString()));");
-    text.push("	}");
-    text.push("}");
+    text.push('	private void assertIO(String input, String output) {');
+    text.push('		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());');
+    text.push('		System.setIn(in);');
+    text.push('');
+    text.push('		ByteArrayOutputStream out = new ByteArrayOutputStream();');
+    text.push('		System.setOut(new PrintStream(out));');
+    text.push('');
+    text.push('		Main.main(new String[0]);');
+    text.push('');
+    text.push('		Assert.assertThat(output + System.lineSeparator(), is(out.toString()));');
+    text.push('	}');
+    text.push('}');
     
     return text.join("\n");
   };
