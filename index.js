ca0322c43c81d345de7e97d772197fb7341cd49c
YujiSoftware <yuji.software+github@gmail.com>
2015-10-13 00:20:37 +0900

    Change context menu label
    Unit test other than JUnit also it was considering

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var self = require("sdk/self");
 var cm = require("sdk/context-menu");
 
 cm.Item({
-  label: "JUnitテストを生成",
-  accesskey: "J",
+  label: "ユニットテストを生成",
+  accesskey: "U",
   context: cm.URLContext(/.*.contest.atcoder.jp\/tasks\/.*/),
   contentScriptFile: [self.data.url("jquery-2.1.4.min.js"), self.data.url("ac-unit-test.js")],
   onMessage: function(code){
