74bff21f3acb24746cf54dea753e053c4ec9b31f
YujiSoftware <yuji.software+github@gmail.com>
2015-10-14 23:00:43 +0900

    Remove test code

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-var buttons = require("sdk/ui/button/action");
 var clipboard = require("sdk/clipboard");
-var tabs = require("sdk/tabs");
 var self = require("sdk/self");
 
 var cm = require("sdk/context-menu");
@@ -14,18 +12,3 @@ cm.Item({
     clipboard.set(code);
   }
 });
-
-var button = buttons.ActionButton({
-  id: "mozilla-link",
-  label: "Visit Mozilla",
-  icon: {
-    "16": "./icon-16.png",
-    "32": "./icon-32.png",
-    "64": "./icon-64.png"
-  },
-  onClick: handleClick
-});
-
-function handleClick(state) {
-  tabs.open("http://abc023.contest.atcoder.jp/assignments");
-}
