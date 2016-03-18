ef85ff1c7d218e1af52fa8f0b80cfb3ea44e4c17
YujiSoftware <yuji.software+github@gmail.com>
2016-03-19 01:52:55 +0900

    Improve question parsing
    ARC034などのように、"#task-statement span span" という入れ子になっている場合に対応

--- data/ac-unit-test.js
+++ data/ac-unit-test.js
@@ -1,41 +1,40 @@
 self.on("click", function(){
-  // 問題全体を取得
-  var statement = $("div#task-statement");
-
-  // なぜか div#task-statement が二重になっているので、子を確認
-  // （大半は二重になっているが、arc046_a だけ二重になっていなかった）
-  var inner = statement.find("div#task-statement");
-  if(inner.length > 0){
-    statement = inner;
-  }
-
   var name = null;
   var input = null;
   var output = null;
   var io = [];
 
-  var children = statement.children();
-  for(var i = 0; i < children.length; i++){
-    var child = $(children[i]);
-    if(child.hasClass("part") || child.get(0).tagName == "SECTION"){
-      var h3 = child.find("h3");
-      var pre = child.find("pre");
-      if(h3.length > 0 && pre.length > 0){
-        var header = h3[0].textContent;
-        var example = pre[0].textContent;
-      
-        if(header.indexOf("入力例") == 0){
-          name = header.replace(/\s+/g, "_");
-          input = example;
-        }else if(header.indexOf("出力例") == 0){
-          output = example;
-        }
+  var sections = $("#task-statement section");
+  for(var i = 0; i < sections.length; i++){
+    var section = $(sections[i]);
+  
+    var h3 = section.find("h3");
+    var pre = section.find("pre");
+
+    // SECTION の中に H3 タグがある場合と、
+    // SECTION の直前に H3 タグがある場合がある(ARC014_A)
+    if(h3.length == 0){
+      var prev = section.prev();
+      if(prev.length > 0 && prev[0].tagName == "H3"){
+        h3 = prev;
       }
-      
-      if(name != null && input != null && output != null){
-        io.push({ name: name, input: input, output: output });
-        name = input = output = null;
+    }
+    
+    if(h3.length > 0 && pre.length > 0){
+      var header = h3[0].textContent.trim();
+      var example = pre[0].textContent;
+    
+      if(header.indexOf("入力例") == 0){
+        name = header.replace(/\s+/g, "_");
+        input = example;
+      }else if(header.indexOf("出力例") == 0){
+        output = example;
       }
+    }
+    
+    if(name != null && input != null && output != null){
+      io.push({ name: name, input: input, output: output });
+      name = input = output = null;
     }
   }
 
