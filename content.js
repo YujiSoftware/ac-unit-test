005252645fcb38023aa3f9e3421fd5b952323429
YujiSoftware <yuji.software+github@gmail.com>
2024-02-21 00:11:54 +0900

    jQuery 依存を削除

--- content.js
+++ content.js
@@ -31,29 +31,27 @@ function onClick(){
   var output = null;
   var io = [];
 
-  var sections = $("#task-statement section");
-  for(var i = 0; i < sections.length; i++){
-    var section = $(sections[i]);
-  
-    var h3 = section.find("h3");
-    var pre = section.find("pre");
+  var sections = document.querySelectorAll("#task-statement section");
+  for(const section of sections){
+    var h3 = section.querySelector("h3");
+    var pre = section.querySelector("pre");
 
     // SECTION の中に H3 タグがある場合(ABC033_D)と、
     // SECTION の直前に H3 タグがある場合(ARC014_A)がある
-    if(h3.length == 0){
-      var prev = section.prev();
-      if(prev.length > 0 && prev[0].tagName == "H3"){
+    if(h3 == null){
+      var prev = section.previousElementSibling;
+      if(prev != null > 0 && prev.tagName == "H3"){
         h3 = prev;
       }
     }
     
-    if(h3.length > 0 && pre.length > 0 && $(h3[0]).is(":visible")){
-      var header = h3[0].firstChild.textContent.trim();
-      var example = pre[0].textContent;
+    if(h3 != null && pre != null && (h3.offsetWidth || h3.offsetHeight)){
+      var header = h3.firstChild.textContent.trim();
+      var example = pre.textContent;
 
       // シンタックスハイライトされている場合、リスト形式に
       // なっているので、一行ずつ取り出す (ABC007_3、など)
-      var pretty = pre[0].getElementsByTagName("li");
+      var pretty = pre.getElementsByTagName("li");
       if(pretty.length > 0){
         example = "";
         for(var j = 0; j < pretty.length; j++){
