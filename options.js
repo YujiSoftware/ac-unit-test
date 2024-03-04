056c0680437c6703ee253dee36e50429c9a6e89d
YujiSoftware <yuji.software+github@gmail.com>
2024-03-04 21:34:21 +0900

    ブラウザのテーマに対応

--- options.js
+++ options.js
@@ -209,6 +209,16 @@ async function initialize() {
         }
     });
 
+    const setTheme = function () {
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.documentElement.setAttribute('data-bs-theme', 'dark')
+        } else {
+            document.documentElement.setAttribute('data-bs-theme', 'light')
+        }
+    };
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', setTheme);
+    setTheme();
+
     document.getElementById("loadPython").addEventListener("click", loadPython);
     document.getElementById("loadJava").addEventListener("click", loadJava);
     document.getElementById("loadKotlin").addEventListener("click", loadKotlin);
